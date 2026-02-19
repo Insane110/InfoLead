bdb87f266d0c1cf2e17287e4bda69c6f206bf051
Insane110 <118113732+Insane110@users.noreply.github.com>
2026-02-19 21:56:11 +0530

    Refactor get_token_nodes to handle sentence nodes
    Refactor get_token_nodes function to process sentence nodes and update metadata.

--- chunk.py
+++ chunk.py
@@ -1,8 +1,7 @@
 from llama_index.core import Document
 from llama_index.core.node_parser import SentenceSplitter, TokenTextSplitter
-from pprint import pprint
 
-def get_token_nodes(documents: list[Document]):
+def get_token_nodes(documents: list[Document]):  
     character_splitter = SentenceSplitter(
         chunk_size=500,
         chunk_overlap=0,
@@ -14,8 +13,15 @@ def get_token_nodes(documents: list[Document]):
         chunk_overlap=20,   
     )
 
-    nodes = character_splitter.get_nodes_from_documents(documents)
-    token_nodes = token_splitter.get_nodes_from_documents(nodes) 
-
+    sentence_nodes = character_splitter.get_nodes_from_documents(documents)
+    token_nodes = []
+    for i, node in enumerate(sentence_nodes):
+        node_text = node.get_content()
+        temp_doc = Document(text=node_text, metadata=node.metadata)
+        sub_nodes = token_splitter.get_nodes_from_documents([temp_doc])
+        for sub_node in sub_nodes:
+            sub_node.metadata.update(node.metadata)
+        
+        token_nodes.extend(sub_nodes)
+    
     return token_nodes
-
